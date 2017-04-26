1524519f38457dc5205bf5ece8f06c6afccc287c
Windtalkers423 <chodsukkasem@kkumail.com>
2017-04-27 03:09:54 +0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>LENS ME | IMAGINE YOUR LIFE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/css/bootstrap.css" rel="stylesheet">
+  <link href="css/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet"> 
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/lightbox.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+  <link rel="stylesheet" href="animate/animate.css" />
+  <link rel="stylesheet" href="animate/set.css" />
+  
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="stylesheet" href="gallery/blueimp-gallery.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    /* Remove the navbar's default rounded borders and increase the bottom margin */ 
+    .navbar {
+      margin-bottom: 50px;
+      border-radius: 0;
+    }
+    
+    /* Remove the jumbotron's default bottom margin */ 
+     .jumbotron {
+      margin-bottom: 0;
+    }
+   
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f4f2f2;
+      padding: 25px;
+    }
+	#bordview {
+		border: 2px solid grey;
+	}
+  </style>
+</head>
+<body>
+
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1><b>CAMERA STORE</b></h1> 
+		<b>EVERY SHOT IS MEMORY</b>
+    <p>NIKON  •&nbsp;&nbsp;&nbsp;&nbsp;CANON  •&nbsp;&nbsp;&nbsp;&nbsp;PANASONIC •&nbsp;&nbsp;&nbsp;&nbsp;OLYMPUS  •&nbsp;&nbsp;&nbsp;&nbsp;SONY  •&nbsp;&nbsp;&nbsp;&nbsp;FUJIFILM</p> 
+  </div>
+</div>
+
+
+
+<header id="home">
+	  
+      <div class="main-nav">
+	  	
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">
+            <h1><img class="img-responsive" src="logo.png" alt="logo"></h1>
+          </a>                    
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">                 
+            <li class="scroll active"><a href="#home">HOME</a></li>
+            <li class="scroll"><a href="#works">PRODUCTS</a></li> 
+            <li class="scroll"><a href="#about-us">ABOUT US</a></li>                     
+            <li class="scroll"><a href="#login">LOGIN</a></li>
+                 
+          </ul>
+        </div>
+      </div>
+    </div><!--/#main-nav-->
+  </header><br><br><br><!--/#home-->
+
+
+
+<!-- ตาราง Spec ข้อมูล -->
+
+<iframe src="https://www.youtube.com/embed/LSb1f_15ev0" width="557" height="360" frameborder="0" allowfullscreen="allowfullscreen"></iframe> <!-- Video สินค้า -->
+</div>
+															<div class="spec">
+					<table>
+						<tr>
+							<th colspan="2">Specification</th>
+						</tr>
+																<tr class="odd">
+							<td>ความละเอียดภาพสูงสุด (Max Resolution)</td>
+							<td>5568 x 3712</td>
+						</tr>
+																	<tr class="even">
+							<td>สัดส่วนภาพ (Image Ratio)</td>
+							<td>3:2, 4:5</td>
+						</tr>
+																	<tr class="odd">
+							<td>พิกเซล (Effective Pixels)</td>
+							<td>20.8 Megapixel</td>
+						</tr>
+																	<tr class="even">
+							<td>เซลล์รับภาพ (Image Sensor)</td>
+							<td>CMOS, 35.9 x 23.9 mm/Full-Frame (1.0x Crop Factor)</td>
+						</tr>
+																	<tr class="odd">
+							<td>ความไวแสง (ISO Rating)</td>
+							<td>Auto, 100-102400 (Extended Mode: 50-3280000)</td>
+						</tr>
+																	<tr class="even">
+							<td>Optical Zoom</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="odd">
+							<td>Digital Zoom</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="even">
+							<td>ระบบป้องกันภาพสั่น (Image Stabilization)</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="odd">
+							<td>ระบบโฟกัส (Auto Focus Type)</td>
+							<td>Auto & Manual</td>
+						</tr>
+																	<tr class="even">
+							<td>ระยะโฟกัสปกติ (Normal Focus Range)</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="odd">
+							<td>ระยะมาโคร (Macro Focus Range)</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="even">
+							<td>White Balance</td>
+							<td>Auto, Cloudy, Color Temperature, Direct Sunlight, Flash, Fluorescent, Incandescent, Preset Manual, Shade</td>
+						</tr>
+																	<tr class="odd">
+							<td>ค่ารูรับแสง (Aperture)</td>
+							<td>n/a</td>
+						</tr>
+																	<tr class="even">
+							<td>ค่าความเร็วชัตเตอร์ (Shutter Speed)</td>
+							<td>30 - 1/8000 second</td>
+						</tr>
+																	<tr class="odd">
+							<td>แฟลชภายใน (Built-In Flash)</td>
+							<td>No</td>
+						</tr>
+																	<tr class="even">
+							<td>ช่องต่อแฟลชภายนอก (External Flash)</td>
+							<td>Hot Shoe, PC Terminal</td>
+						</tr>
+																	<tr class="odd">
+							<td>ระบบแฟลช (Flash Modes)</td>
+							<td>1st Curtain Sync Hi-Speed Sync Off Rear Curtain/Slow Sync Rear Sync Red-eye Reduction Slow Sync Slow Sync/Red-eye Reduction</td>
+						</tr>
+																	<tr class="even">
+							<td>การชดเชยแสง (Exposure Compensation)</td>
+							<td>±5 (in 1/3, 1/2 or 1 EV steps)</td>
+						</tr>
+																	<tr class="odd">
+							<td>ระบบวัดแสง (Metering)</td>
+							<td>3D Color Matrix Metering, Center-weighted average metering, Spot metering, Highlight Weighted</td>
+						</tr>
+																	<tr class="even">
+							<td>โหมดแมนนวล (Manual Mode)</td>
+							<td>Yes</td>
+						</tr>
+																	<tr class="odd">
+							<td>ถ่ายภาพต่อเนื่อง (Continuous Drive)</td>
+							<td>Up to 14 fps</td>
+						</tr>
+																	<tr class="even">
+							<td>ถ่ายภาพเคลือนไหว (Movie Clips)</td>
+							<td>MOV, MPEG-4 AVC/H.264</td>
+						</tr>
+																	<tr class="odd">
+							<td>ตั้งเวลาถ่ายภาพ (Self-Timer)</td>
+							<td>20 seconds, 10 seconds, 5 seconds, 2 seconds </td>
+						</tr>
+																	<tr class="even">
+							<td>ประเภทหน่วยความจำ (Memory Type)</td>
+							<td>CompactFlash</td>
+						</tr>
+																	<tr class="odd">
+							<td>ไฟล์ภาพ (File Types)</td>
+							<td>JPEG, RAW, TIFF</td>
+						</tr>
+																	<tr class="even">
+							<td>ช่องมองภาพ (View Finder)</td>
+							<td>Pentaprism</td>
+						</tr>
+																	<tr class="odd">
+							<td>ขนาดLCD</td>
+							<td>3.2</td>
+						</tr>
+																	<tr class="even">
+							<td>Video Out</td>
+							<td>1/8</td>
+						</tr>
+																	<tr class="odd">
+							<td>ประเภทแบตเตอรี่ (Battery)</td>
+							<td>1x EN-EL18a Rechargeable Lithium-ion Battery Pack, 10.8 VDC, 2500 mAh</td>
+						</tr>
+																	<tr class="even">
+							<td>น้ำหนัก (Weight)</td>
+							<td>1415 g (3.11 lb)with battery and memory cards</td>
+						</tr>
+																	<tr class="odd">
+							<td>ขนาด (Dimensions)</td>
+							<td>(WxHxD)6.3 x 6.2 x 3.6</td>
+						</tr>
+																</table>
+				</div>
